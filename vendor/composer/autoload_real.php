60da8ceefefba45b37389d2dd086ee940e8c439d
Olivier van Helden <olivier@van-helden.net>
2022-08-31 10:32:18 -0400

    fix missing meta-box autoload files

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -52,6 +52,29 @@ class ComposerAutoloaderInitfe6292387e56d846764579a2c23c0495
 
         $loader->register(true);
 
+        if ($useStaticLoader) {
+            $includeFiles = Composer\Autoload\ComposerStaticInitfe6292387e56d846764579a2c23c0495::$files;
+        } else {
+            $includeFiles = require __DIR__ . '/autoload_files.php';
+        }
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequirefe6292387e56d846764579a2c23c0495($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequirefe6292387e56d846764579a2c23c0495($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,6 +6,18 @@ namespace Composer\Autoload;
 
 class ComposerStaticInitfe6292387e56d846764579a2c23c0495
 {
+    public static $files = array (
+        '23e92b0e70bc71ebe8e5a382f1a5652a' => __DIR__ . '/..' . '/meta-box/meta-box/meta-box.php',
+        '8771acd7754cd5526bb29fae66e89026' => __DIR__ . '/..' . '/meta-box/meta-box-columns/meta-box-columns.php',
+        '3d8ae901d95bb0104a1c51414a8c5d1d' => __DIR__ . '/..' . '/meta-box/meta-box-conditional-logic/meta-box-conditional-logic.php',
+        '4f0709d7d5ca45c2d93532f309176335' => __DIR__ . '/..' . '/meta-box/meta-box-group/meta-box-group.php',
+        '1688fd0706033116c32103002045d66b' => __DIR__ . '/..' . '/meta-box/meta-box-include-exclude/meta-box-include-exclude.php',
+        '1d84e39720398ae69aabb7481a1095fb' => __DIR__ . '/..' . '/meta-box/meta-box-show-hide/meta-box-show-hide.php',
+        '77ea69ad6ea6c44c88696aa6d206c4e9' => __DIR__ . '/..' . '/meta-box/mb-admin-columns/mb-admin-columns.php',
+        'dea16cc47f4d4e803c3d58d1238c32a7' => __DIR__ . '/..' . '/meta-box/mb-settings-page/mb-settings-page.php',
+        'ad795f737f50665a589664d97d9ec553' => __DIR__ . '/..' . '/meta-box/meta-box-tabs/meta-box-tabs.php',
+    );
+
     public static $prefixLengthsPsr4 = array (
         'P' => 
         array (
