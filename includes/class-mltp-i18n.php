7235f1fa20867d0bd8b63d5c65bfdf5cbfd263cb
Olivier van Helden <olivier@van-helden.net>
2022-09-29 09:38:11 -0400

    Workaround to fix localisation bug in mb-admin-columns module

--- includes/class-mltp-i18n.php
+++ includes/class-mltp-i18n.php
@@ -39,8 +39,53 @@ class Mltp_I18n {
 			dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/'
 		);
 
+		$filters = array(
+			array(
+				'hook'          => 'wp_dropdown_cats',
+				'callback'      => 'fix_mb_show_option_all',
+				'accepted_args' => 2,
+			),
+			// array(
+			// 'hook' => 'list_cats',
+			// 'callback' => 'list_cats_filter',
+			// 'accepted_args' => 2,
+			// ),
+		);
+
+		foreach ( $filters as $hook ) {
+			$hook = array_merge(
+				array(
+					'component'     => $this,
+					'priority'      => 10,
+					'accepted_args' => 1,
+				),
+				$hook
+			);
+			add_filter( $hook['hook'], array( $hook['component'], $hook['callback'] ), $hook['priority'], $hook['accepted_args'] );
+		}
+
 	}
 
+	/**
+	 * Workaround to fix localisation bug in mb-admin-columns module.
+	 * Set placeholder to $taxonomy->labels->all_items instead of _('All %s').
+	 *
+	 * @param string $output        Html for filter taxonomy dropdown menu.
+	 * @param array  $parsed_args   Context elements.
+	 * @return string               Fixed html.
+	 */
+	public function fix_mb_show_option_all( $output, $parsed_args ) {
+		$taxonomy = get_taxonomy( $parsed_args['taxonomy'] );
+		if ( ! $taxonomy ) {
+			return $output;
+		}
 
+		$show_option_all = ( empty( $taxonomy->labels->show_option_all ) ) ? $taxonomy->labels->all_items : $taxonomy->labels->show_option_all;
+		if ( empty( $show_option_all ) ) {
+			return $output;
+		}
+
+		return preg_replace( ":<option value='0'>.*</option>:", "<option value='0'>$show_option_all</option>", $output );
+	}
 
 }
