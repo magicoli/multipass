3cba2493ecfecb268e1772f1db56fe2fa7f10e53
Olivier van Helden <olivier@van-helden.net>
2022-09-30 15:44:10 -0400

    fix calendar loading spin not spinning

--- includes/js/calendar.asset.php
+++ includes/js/calendar.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n'), 'version' => '6ea0f16073cabf59e685');
+<?php return array('dependencies' => array('wp-i18n'), 'version' => '3756f977da392c99a425');
