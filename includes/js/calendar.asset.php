57305cb781424e396531c3e16e64816030ee663b
Olivier van Helden <olivier@van-helden.net>
2022-10-03 15:57:16 -0400

    added Airbnb and Expedia signs to calendar display

--- includes/js/calendar.asset.php
+++ includes/js/calendar.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-i18n'), 'version' => '531a9341a31b26d19915');
+<?php return array('dependencies' => array('wp-i18n'), 'version' => 'b3e8f6262bdfa6704cd0');
