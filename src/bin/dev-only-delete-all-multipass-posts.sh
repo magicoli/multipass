1c7ec7160cd691853a61a8e94282b464c4ba084b
Olivier van Helden <olivier@van-helden.net>
2022-10-04 18:42:58 -0400

    dev script to delete all multipass posts

---
+++ src/bin/dev-only-delete-all-multipass-posts.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+wp post-type list --format=csv | cut -d , -f 1 | sort | egrep "^mp_|mltp|prestation|resource"
+
+for post_type in prestation prestation-item mp_resource
+do
+  count=$(wp db query "select count(*) from wp_posts where post_type='$post_type'" | tail +2)
+  echo "# removein $count $post_type posts" >&2
+  wp post delete --force $(wp post list --post_type=$post_type --format=ids)
+  count=$(wp db query "select count(*) from wp_posts where post_type='$post_type'" | tail +2)
+  echo "  remaining: $count" >&2
+done
+
+# wp post delete --force $(wp post list --post_type='prestation_part' --format=ids)
+# wp post delete --force $(wp post list --post_type='prestation-part' --format=ids)
+# wp post delete --force $(wp post list --post_type='prestationpart' --format=ids)
+# wp post delete --force $(wp post list --post_type='			// $source_url = 'https://app.lodgify.com/#/reservation/inbox/B' . $booking['id'];
+# wp post delete --force $(wp post list --post_type='prestation-part' --format=ids)
+# wp post delete --force $(wp post list --post_type='prestation-item' --format=ids)
+# wp post delete --force $(wp post list --post_type='prestation-item' --format=ids)
