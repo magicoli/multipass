magicoli/multipass

commit 37ac13f73c89d7ad330da5586df3353d1ee78acd

fix woocommerce edit_url and source_edit_url

includes/modules/class-mltp-woocommerce.php
@@ -644,12 +644,12 @@ class Mltp_WooCommerce extends Mltp_Modules {
 
 			$args = array(
 				'source'           => 'woocommerce',
-				'woocommerce_uuid' => $uuid,
-				'date'             => $post->post_date,
 				'source_id'        => "$post_id",
 				'source_item_id'   => "$item_id",
+				'source_edit_url'         => $order->get_edit_order_url(),
+				// 'woocommerce_uuid' => $uuid,
+				'date'             => $post->post_date,
 				// 'view_url'         => $order->get_view_order_url(),
-				'edit_url'         => $order->get_edit_order_url(),
 				'resource_id'      => self::get_resource( $product_id ),
 
 				'description'      => "$description",

commit d4b7a2762e8363dabd0e9bd76ca62fa4b936a3be

Fix woocommerce missing imports (regression in  37ac13f7)

includes/class-mltp-prestation-detail.php
@@ -1328,6 +1328,7 @@ class Mltp_Item {
 					return false;
 				}
 				$source = $args['source'];
+				$source_id = $args['source_id'];
 				$uuid_field = $args['source'] . '_uuid';
 
 				$source_term_id = get_term_by( 'slug', $args['source'], 'mltp_detail-source' );

includes/modules/class-mltp-woocommerce.php
@@ -561,8 +561,6 @@ class Mltp_WooCommerce extends Mltp_Modules {
 
 		$order = wc_get_order( $post_id ); // make sure it is a wc object, not only a post
 		foreach ( $order->get_items() as $item_id => $item ) {
-			// $uuid = wp_hash( 'woocommerce-' . get_current_blog_id() ) . '-' . $post_id . '-'  . $item_id;
-			$uuid       = join( '-', array( get_current_blog_id(), $post_id, $item_id ) );
 			$product    = $item->get_product();
 			$product_id = $product->get_id();
 
@@ -644,12 +642,12 @@ class Mltp_WooCommerce extends Mltp_Modules {
 
 			$args = array(
 				'source'           => 'woocommerce',
-				'source_id'        => "$post_id",
-				'source_item_id'   => "$item_id",
-				'source_edit_url'         => $order->get_edit_order_url(),
-				// 'woocommerce_uuid' => $uuid,
+				'source_id'        => $post_id,
+				'woocommerce_uuid' => MultiPass::hash_source_uuid('woocommerce', $post_id, $item_id),
 				'date'             => $post->post_date,
+				'source_item_id'   => "$item_id",
 				// 'view_url'         => $order->get_view_order_url(),
+				// 'edit_url'         => $order->get_edit_order_url(),
 				'resource_id'      => self::get_resource( $product_id ),
 
 				'description'      => "$description",