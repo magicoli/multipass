88cf5bdbf559b6dc68f1ecfd8763c7b56537aa5b
Olivier van Helden <olivier@van-helden.net>
2024-03-31 11:09:46 -0400

    set minimum to date also in prestation details list

--- src/admin/edit-prestation.js
+++ src/admin/edit-prestation.js
@@ -14,5 +14,25 @@ jQuery(document).ready(function($) {
                 $('#dates_to').datepicker('show');
             }, 100);
         });
+
+        // Attacher l'événement change aux champs from et to même s'ils sont ajoutés après le chargement de la page
+        $(document).on('change', 'input[id^="manual_items_from"]', function() {
+            var i = $('input[id^="manual_items_from"]').index(this);
+            // Récupérer la date de début
+            var startDate = $(this).val();
+
+            // Mettre à jour la date minimale pour la date de fin
+            $('input[id^="manual_items_to"]').eq(i).datepicker('option', 'minDate', startDate);
+
+            // Déplacer le focus vers le champ date_to après un court délai
+            setTimeout(function() {
+                $('input[id^="manual_items_to"]').eq(i).datepicker('show');
+            }, 100);
+        });
+
+        // Écouter les modifications sur #mltp_details et mettre à jour les actions en conséquence
+        $('#mltp_details').change(function() {
+            // Mettre à jour les actions ici
+        });
     }
 });
